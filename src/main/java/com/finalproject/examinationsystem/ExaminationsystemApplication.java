3969eac6078fc3214c87e896fdc2d60f5d0f22f6
sevgiaydn <sevgiaydin1999@hotmail.com>
2021-02-07 17:16:59 +0300

    deneme
    ilk uygulama

---
+++ src/main/java/com/finalproject/examinationsystem/ExaminationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.examinationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExaminationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/examinationsystem/ExaminationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.examinationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
