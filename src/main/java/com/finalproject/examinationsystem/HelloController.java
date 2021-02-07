b75244d602221437567aab57a7deedc3973a058c
yaren <yarenbergi25@gmail.com>
2021-02-07 18:04:14 +0300

    Update HelloController.java
    yaren deneme

--- src/main/java/com/finalproject/examinationsystem/HelloController.java
+++ src/main/java/com/finalproject/examinationsystem/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Sevgi!sssssssssssssssssssssssss";
+        return "Greetings from Sevgi!sssssssssssssssssssssssss hello";
     }
 
 }
